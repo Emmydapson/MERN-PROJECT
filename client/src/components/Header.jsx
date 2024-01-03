30add39fd7ae8a468e73020eeaa6738ddcb1e0f6
emmydapson <iloriemmanuel00@gmail.com>
2024-01-03 12:39:17 +0100

    create user model

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,27 +3,29 @@ import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-blue-500 text-white shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
           <Link to='/'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-              <span className='text-slate-500'>Dapson</span>
-              <span className='text-slate-700'>Estate</span>
+            <h1 className='font-bold text-lg sm:text-xl flex flex-wrap'>
+              <span className='text-yellow-300'>Dapson</span>
+              <span className='text-yellow-100'>Estate</span>
             </h1>
           </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <FaSearch className='text-slate-600' />
+            <form className='bg-white p-2 rounded-lg flex items-center'>
+                <input type="text" 
+                placeholder='Search...' 
+                className='bg-transparent focus:outline-none w-24 sm:w-64 text-gray-800'/>
+                <FaSearch className='text-gray-600' />
             </form>
             <ul className='flex gap-4'>
               <Link to='/'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+              <li className='hidden sm:inline  hover:text-yellow-300'>Home</li>
               </Link>
               <Link to='/about'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+              <li className='hidden sm:inline hover:text-yellow-300'>About</li>
               </Link>
               <Link to='/sign-in'>
-              <li className='text-slate-700 hover:underline'>Sign in</li> 
+              <li className='hover:text-yellow-300'>Sign in</li> 
               </Link>             
             </ul>
         </div>
